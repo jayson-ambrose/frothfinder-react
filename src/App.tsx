4f9fa53b2483cbfe6e2b097d6ede05661ed44583
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-11-26 18:34:38 -0700

    logo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import '../public/logo.png'
+
 function App() {
 
   return (
     <>
-      <h1 className="text-2xl">Hello World</h1>
+    <div className='bg-black py-10 flex flex-col items-center'>
+      <img className='h-72 w-auto' src='logo.png' alt='frothfinder logo'/>
+    </div>
+    <h1 className="text-2xl">Hello World</h1>
     </>
   )
 }

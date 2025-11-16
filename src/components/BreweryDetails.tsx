0ec6cf80fbc70a6a5834354af9c938e532afd6f8
jayson ambrose <jayson.ambrose.87@gmail.com>
2025-11-16 12:13:00 -0700

    added directions via google maps

--- src/components/BreweryDetails.tsx
+++ src/components/BreweryDetails.tsx
@@ -1,6 +1,6 @@
 export default function BreweryDetails ({brewery}: {brewery: any}) {
     
-    const {id, street, city, state, postal_code, name, phone, website_url} = brewery
+    const {id, street, city, state, postal_code, name, phone, website_url, latitude, longitude} = brewery
 
     function formatPhoneNumber(phoneString: string) {
 
@@ -14,11 +14,15 @@ export default function BreweryDetails ({brewery}: {brewery: any}) {
 
     return (
         <div>
-            <h1 className="font-bold text-xl">{name}</h1>
+            <h1 className="font-bold text-2xl">{name}</h1>
             <h1>{street}</h1>
             <h1>{city}, {state} {postal_code}</h1>
-            <h1>{formatPhoneNumber(phone)}</h1>
-            <a href={website_url} target="blank">{website_url}</a>
+            <h1 className='font-bold text-lg'>Contact Info and Website</h1>
+            <h1>{phone ? formatPhoneNumber(phone) : null}</h1>
+            <h1>{website_url}</h1>
+            <a target='blank' href={`https://www.google.com/maps/dir/?api=1&destination=${street}+${city}+${state}`} className='text-blue-500'>
+                Get Directions
+            </a>
         </div>
     )
 

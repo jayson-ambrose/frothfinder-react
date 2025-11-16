ce7cb6850aa1c6c56e6c1f567c68ba3c1f42cb96
jayson ambrose <jayson.ambrose.87@gmail.com>
2025-11-16 11:16:45 -0700

    formatting work on details

--- src/components/BreweryDetails.tsx
+++ src/components/BreweryDetails.tsx
@@ -1,9 +1,24 @@
 export default function BreweryDetails ({brewery}: {brewery: any}) {
+    
+    const {id, street, city, state, postal_code, name, phone, website_url} = brewery
+
+    function formatPhoneNumber(phoneString: string) {
+
+        if (phoneString.length !== 10){
+            throw new Error('unable to format phone number'
+            )
+        }
+
+        return `(${phoneString.slice(0, 3)})${phoneString.slice(3, 6)}-${phoneString.slice(6)}`
+    } 
 
     return (
         <div>
-            <h1>{brewery.name}</h1>
-            <h1>{brewery.id}</h1>
+            <h1 className="font-bold text-xl">{name}</h1>
+            <h1>{street}</h1>
+            <h1>{city}, {state} {postal_code}</h1>
+            <h1>{formatPhoneNumber(phone)}</h1>
+            <a href={website_url} target="blank">{website_url}</a>
         </div>
     )
 
